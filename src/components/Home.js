281ad6eb3c07b7a5abc6d15e6c3f9b979a014884
Tamesna-Lamyae <lamyae.tamesna@uit.ac.ma>
2024-12-06 12:52:44 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+export default function Home() {
+  return `
+    <section 
+      class="p-8 bg-cover bg-center text-black h-screen flex items-center"
+      style="background-image: url('https://www.kamkamdunes.com/wp-content/uploads/2023/05/platillos-tipicos-de-marruecos-2.jpg');"
+    >
+      <div class="container mx-auto bg-yellow-50 text-yellow-900 bg-opacity-70 p-8 rounded-lg shadow-lg">
+        <h1 class="text-4xl  font-extrabold mb-4 text-center font-arabic">
+          Welcome to the Moroccan Cuisine Blog
+        </h1>
+        <p class="text-lg leading-relaxed text-center  font-arabic">
+          Welcome to <strong>Kozintk</strong>, your gateway to the rich and diverse world of Moroccan cuisine! 
+          Here, we share authentic recipes from Morocco, covering traditional dishes such as tajines, couscous, pastilla, and more. 
+          Each recipe includes a detailed list of ingredients and step-by-step preparation instructions, ensuring 
+          that you can bring the flavors of Morocco into your own kitchen. 
+          Dive in and explore the vibrant culinary heritage that has been passed down for generations.
+        </p>
+      </div>
+    </section>
+  `;
+}
+
